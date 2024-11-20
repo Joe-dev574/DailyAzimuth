2923b985fd335d3418832c6703cdce867d0132bc
JDeWeese <jdeweese574@icloud.com>
2024-11-20 10:41:27 -0600

    Initial Commit

---
+++ DailyAzimuthTests/DailyAzimuthTests.swift
@@ -0,0 +1,17 @@
+//
+//  DailyAzimuthTests.swift
+//  DailyAzimuthTests
+//
+//  Created by Joseph DeWeese on 11/20/24.
+//
+
+import Testing
+@testable import DailyAzimuth
+
+struct DailyAzimuthTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
